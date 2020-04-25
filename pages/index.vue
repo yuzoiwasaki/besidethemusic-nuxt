45ace7f5e6b96421678c757189c2e9e453c71424
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-04-25 17:04:59 +0900

    Create ProjectCard component

--- pages/index.vue
+++ pages/index.vue
@@ -39,123 +39,71 @@
           </div>
         </div>
         <div class="columns is-multiline">
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              思い出野郎Aチーム presents ソウルピクニック・ファンディング
-            </p>
-            <p class="subtitle is-6">
-              誰もが集まれる場所を守るために。思い出野郎Aチームが主催する支援プロジェクト「ソウルピクニック・ファンディング」
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/261880/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              UNDERBAR UNDERDEERLOUNGE支援プロジェクト
-            </p>
-            <p class="subtitle is-6">
-              コロナウィルス拡散防止のため４月より営業自粛を開始。現状再開時期の目処はついておらず、そんな中大変心苦しい決断ではありましたがコロナウィルスの脅威がなくなった時に、新たなスタートを切れるよう皆様からの支援を募ることを決めました。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/255922/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              【 SAVE THE Organ bar 】Organ bar 営業活動支援
-            </p>
-            <p class="subtitle is-6">
-              新型コロナウイルス感染拡大防止の為、4/1より営業を自粛しておりますが、お客様や関係者の皆様には１日も早く終息に向かい、平穏無事な日常が戻ることを祈っております。そこで大変心苦しいお願いではありますが、休業に伴う緊急支援をお願いさせて頂きます。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/258853/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              東京都八王子市のライブハウス、八王子papaBeatを継続させたい。
-            </p>
-            <p class="subtitle is-6">
-              新型コロナの影響により、存続の危機を迎えています。私たちにとって大切なこの場所を、なんとか存続させたい。一方で、自粛要請にも「応じたい」とも考えています。ライブハウスがクラスター感染の発生源になることは、決して望んではいません。「自粛」と「存続」の両立のため、ご支援を募ることとしました。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/252263/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              代々木ライブハウス救済プロジェクト
-            </p>
-            <p class="subtitle is-6">
-              代々木で運営している経営母体の違うライブハウスを支援するプロジェクト対象となる会場は下記となります。 1.LIVE labo YOYOGI 2.Zher the ZOO YOYOGI 3.アルティカセブン 4.ライブハウス代々木Barbara
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/256767/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              SAVE OUR SHOW! 珈琲美学～緊急支援！またこの場所でライブをやろう！
-            </p>
-            <p class="subtitle is-6">
-              目黒区学芸大学駅近くにあるライブハウス「珈琲美学」。42年間ご夫婦で営業しているこの店は日頃から若いミュージシャンからプロのミュージシャンのライブをサポート。いつも全力で音楽を愛する人々の憩いの場、表現の場としてお店を営業。高齢のママとマスターを休業の間支援したいと思いこの企画を立ち上げました。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/251951/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              【ライブハウス救済プロジェクト】SAVE THE S.U.B TOKYO
-            </p>
-            <p class="subtitle is-6">
-              東京都中野坂上にあるライブハウスS.U.B TOKYOです。これまで様々なアイドル様アーティスト様にご利用頂いております。現在S.U.B TOKYOはコロナウイルス感染拡大防止のための政府からの自粛要請を受けまして、営業を自粛している状態です。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/260211/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              西荻窪w.jaz コロナを乗り越える！支援プロジェクト
-            </p>
-            <p class="subtitle is-6">
-              西荻窪w.jaz 通称だぶじゃず。コロナウィルスが猛威を振るう中、3月末から営業自粛しています。先が見えないこの状況ですが、必ず再起したいという思いで、大変心苦しいですが支援をお願いした次第です。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/258861/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              大久保bumpcity -バンプシティ- コロナに負けない！応援プロジェクト！
-            </p>
-            <p class="subtitle is-6">
-              大久保にあるライブバー、Bumpcity。バンプの名称でおなじみのあのバンプシティを救済するための応援プロジェクトです。13年目を迎えたバンプの火をまだまだ消したくない！皆さんの応援をお願いします。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/252906/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              Live Bar BACK BEATを、コロナ被害から助けよう！
-            </p>
-            <p class="subtitle is-6">
-              新型コロナウィルスの感染防止のため、営業自粛してます。その間にも、家賃やレンタルリース代等、いろいろお金はかかります。今後の営業存続のためにも、ご協力をよろしくお願い申し上げます。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/261373/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              LivePerformanceBar un.10応援プロジェクト
-            </p>
-            <p class="subtitle is-6">
-              コロナウィルスの影響により、今週から営業を自粛いたします四ツ谷荒木町に店を構えるLivePerformanceBar un.10の応援事業です。変わらず音楽を自由に楽しめる場所を失くさないために、どうぞよろしくお願いいたします。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/259532/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              三軒茶屋グレープフルーツムーン　緊急支援クラウドファンディング
-            </p>
-            <p class="subtitle is-6">
-              20年続いているGrapeFruitMoonの灯を消さない為に。これからも変らず皆様と笑顔溢れる空間を一緒に過ごせるように。今回のクラウドファンディングを立ち上げました。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/105794/widget" width="245"></iframe>
-          </div>
-          <div class="column is-3 has-padding-30">
-            <p class="title is-5 is-spaced">
-              ”STAY STRONG” 【新宿 DREAM STORE】支援プロジェクト
-            </p>
-            <p class="subtitle is-6">
-              私たちが大好きな【新宿 DREAM STORE】は新型コロナウィルス感染拡大防止のため営業することが出来ず、危機的な経営状況となっています。多くのライブハウスが店を潰さないように配信ライブなどの新しい体制を模索していますが、少しの期間、皆様の支援を必要としています。
-            </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/261846/widget" width="245"></iframe>
-          </div>
+          <ProjectCard
+            title="思い出野郎Aチーム presents ソウルピクニック・ファンディング"
+            caption="誰もが集まれる場所を守るために。思い出野郎Aチームが主催する支援プロジェクト「ソウルピクニック・ファンディング」"
+            project-id="261880"
+          />
+          <ProjectCard
+            title="UNDERBAR UNDERDEERLOUNGE支援プロジェクト"
+            caption="コロナウィルス拡散防止のため４月より営業自粛を開始。現状再開時期の目処はついておらず、そんな中大変心苦しい決断ではありましたがコロナウィルスの脅威がなくなった時に、新たなスタートを切れるよう皆様からの支援を募ることを決めました。"
+            project-id="255922"
+          />
+          <ProjectCard
+            title="【 SAVE THE Organ bar 】Organ bar 営業活動支援"
+            caption="新型コロナウイルス感染拡大防止の為、4/1より営業を自粛しておりますが、お客様や関係者の皆様には１日も早く終息に向かい、平穏無事な日常が戻ることを祈っております。そこで大変心苦しいお願いではありますが、休業に伴う緊急支援をお願いさせて頂きます。"
+            project-id="258853"
+          />
+          <ProjectCard
+            title="東京都八王子市のライブハウス、八王子papaBeatを継続させたい。"
+            caption="新型コロナの影響により、存続の危機を迎えています。私たちにとって大切なこの場所を、なんとか存続させたい。一方で、自粛要請にも「応じたい」とも考えています。ライブハウスがクラスター感染の発生源になることは、決して望んではいません。「自粛」と「存続」の両立のため、ご支援を募ることとしました。"
+            project-id="252263"
+          />
+          <ProjectCard
+            title="代々木ライブハウス救済プロジェクト"
+            caption="代々木で運営している経営母体の違うライブハウスを支援するプロジェクト対象となる会場は下記となります。 1.LIVE labo YOYOGI 2.Zher the ZOO YOYOGI 3.アルティカセブン 4.ライブハウス代々木Barbara"
+            project-id="256767"
+          />
+          <ProjectCard
+            title="SAVE OUR SHOW! 珈琲美学～緊急支援！またこの場所でライブをやろう！"
+            caption="目黒区学芸大学駅近くにあるライブハウス「珈琲美学」。42年間ご夫婦で営業しているこの店は日頃から若いミュージシャンからプロのミュージシャンのライブをサポート。いつも全力で音楽を愛する人々の憩いの場、表現の場としてお店を営業。高齢のママとマスターを休業の間支援したいと思いこの企画を立ち上げました。"
+            project-id="251951"
+          />
+          <ProjectCard
+            title="【ライブハウス救済プロジェクト】SAVE THE S.U.B TOKYO"
+            caption="東京都中野坂上にあるライブハウスS.U.B TOKYOです。これまで様々なアイドル様アーティスト様にご利用頂いております。現在S.U.BTOKYOはコロナウイルス感染拡大防止のための政府からの自粛要請を受けまして、営業を自粛している状態です。"
+            project-id="260211"
+          />
+          <ProjectCard
+            title="西荻窪w.jaz コロナを乗り越える！支援プロジェクト"
+            caption="西荻窪w.jaz 通称だぶじゃず。コロナウィルスが猛威を振るう中、3月末から営業自粛しています。先が見えないこの状況ですが、必ず再起したいという思いで、大変心苦しいですが支援をお願いした次第です。"
+            project-id="258861"
+          />
+          <ProjectCard
+            title="大久保bumpcity -バンプシティ- コロナに負けない！応援プロジェクト！"
+            caption="大久保にあるライブバー、Bumpcity。バンプの名称でおなじみのあのバンプシティを救済するための応援プロジェクトです。13年目を迎えたバンプの火をまだまだ消したくない！皆さんの応援をお願いします。"
+            project-id="252906"
+          />
+          <ProjectCard
+            title="Live Bar BACK BEATを、コロナ被害から助けよう！"
+            caption="新型コロナウィルスの感染防止のため、営業自粛してます。その間にも、家賃やレンタルリース代等、いろいろお金はかかります。今後の営業存続のためにも、ご協力をよろしくお願い申し上げます。"
+            project-id="261373"
+          />
+          <ProjectCard
+            title="LivePerformanceBar un.10応援プロジェクト"
+            caption="コロナウィルスの影響により、今週から営業を自粛いたします四ツ谷荒木町に店を構えるLivePerformanceBar un.10の応援事業です。変わらず音楽を自由に楽しめる場所を失くさないために、どうぞよろしくお願いいたします。"
+            project-id="259532"
+          />
+          <ProjectCard
+            title="三軒茶屋グレープフルーツムーン　緊急支援クラウドファンディング"
+            caption="20年続いているGrapeFruitMoonの灯を消さない為に。これからも変らず皆様と笑顔溢れる空間を一緒に過ごせるように。今回のクラウドファンディングを立ち上げました。"
+            project-id="105794"
+          />
+          <ProjectCard
+            title="”STAY STRONG” 【新宿 DREAM STORE】支援プロジェクト"
+            caption="私たちが大好きな【新宿 DREAM STORE】は新型コロナウィルス感染拡大防止のため営業することが出来ず、危機的な経営状況となっています。多くのライブハウスが店を潰さないように配信ライブなどの新しい体制を模索していますが、少しの期間、皆様の支援を必要としています。"
+            project-id="261846"
+          />
         </div>
       </div>
     </section>
@@ -163,6 +111,13 @@
 </template>
 
 <script>
+import ProjectCard from '~/components/ProjectCard.vue'
+
+export default {
+  components: {
+    ProjectCard
+  }
+}
 </script>
 
 <style>
@@ -179,9 +134,5 @@
 
 .body-title {
   margin-bottom: 5rem;
-}
-
-.has-padding-30 {
-  padding: 30px !important;
 }
 </style>
