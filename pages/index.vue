7b4b21e5983c5a9dc2e0165cde5557fe3f3bbd8e
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-04-25 15:07:00 +0900

    Add some contents

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <div class="hero is-success is-medium">
         <div class="hero-body">
           <div class="container is-small has-text-centered">
-            <h1 class="title is-2 is-spaced">
+            <h1 class="title is-1 is-spaced">
               Beside the Music
             </h1>
             <h2 class="subtitle">
@@ -15,42 +15,119 @@
       </div>
     </section>
 
+    <section class="section has-background-light">
+      <div class="container is-small has-text-centered">
+        <div class="columns">
+          <div class="column is-half is-offset-one-quarter">
+            <p>
+            現在コロナウイルスの影響で営業することができず、苦しんでいるライブハウスがたくさんあります。中にはすでに店を閉めざるを得ない選択をしたところもあります。<br><br>
+            私自身がそうであったように、音楽などの文化は苦しい時にこそ必要とされるものだと思います。大切な音楽の灯を消さないために、少しでも支援していただけたら嬉しいです。<br><br>
+            誰かにとっての大切な場所がこれからもずっと存続できますように。
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <section class="section has-background-white is-medium">
       <div class="container is-small has-text-centered">
         <div class="columns">
           <div class="column is-half is-offset-one-quarter">
             <h3 class="title is-spaced body-title">
-              現在クラウドファンディング実施中のライブハウス一覧
+              現在クラウドファンディング実施中の東京のライブハウス一覧
             </h3>
           </div>
         </div>
-        <div class="columns">
-          <div class="column">
+        <div class="columns is-multiline">
+          <div class="column is-3 has-padding-30">
             <p class="title is-5 is-spaced">
-              LIVE ROPPONGI BUZZ 救済　プロジェクト
+              UNDERBAR UNDERDEERLOUNGE支援プロジェクト
             </p>
             <p class="subtitle is-6">
-              六本木のライブバー「BUZZ」は新型コロナウィルスの影響による感染拡大防止に努め、3月4日より営業を自粛しています。このままでは廃業しかねない状況に追い込まれています。そこでBUZZ救済を求めて、このプロジェクトを立ち上げました。笑顔溢れる楽しいステージを再び皆様と共有したいと思います。
+              コロナウィルス拡散防止のため４月より営業自粛を開始。現状再開時期の目処はついておらず、そんな中大変心苦しい決断ではありましたがコロナウィルスの脅威がなくなった時に、新たなスタートを切れるよう皆様からの支援を募ることを決めました。
             </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/251897/widget" width="245"></iframe>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/255922/widget" width="245"></iframe>
           </div>
-          <div class="column">
+          <div class="column is-3 has-padding-30">
             <p class="title is-5 is-spaced">
-              LIVE ROPPONGI BUZZ 救済　プロジェクト
+              【 SAVE THE Organ bar 】Organ bar 営業活動支援
             </p>
             <p class="subtitle is-6">
-              六本木のライブバー「BUZZ」は新型コロナウィルスの影響による感染拡大防止に努め、3月4日より営業を自粛しています。このままでは廃業しかねない状況に追い込まれています。そこでBUZZ救済を求めて、このプロジェクトを立ち上げました。笑顔溢れる楽しいステージを再び皆様と共有したいと思います。
+              新型コロナウイルス感染拡大防止の為、4/1より営業を自粛しておりますが、お客様や関係者の皆様には１日も早く終息に向かい、平穏無事な日常が戻ることを祈っております。そこで大変心苦しいお願いではありますが、休業に伴う緊急支援をお願いさせて頂きます。
             </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/251897/widget" width="245"></iframe>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/258853/widget" width="245"></iframe>
           </div>
-          <div class="column">
+          <div class="column is-3 has-padding-30">
             <p class="title is-5 is-spaced">
-              LIVE ROPPONGI BUZZ 救済　プロジェクト
+              代々木ライブハウス救済プロジェクト
             </p>
             <p class="subtitle is-6">
-              六本木のライブバー「BUZZ」は新型コロナウィルスの影響による感染拡大防止に努め、3月4日より営業を自粛しています。このままでは廃業しかねない状況に追い込まれています。そこでBUZZ救済を求めて、このプロジェクトを立ち上げました。笑顔溢れる楽しいステージを再び皆様と共有したいと思います。
+              代々木で運営している経営母体の違うライブハウスを支援するプロジェクト対象となる会場は下記となります。 1.LIVE labo YOYOGI 2.Zher the ZOO YOYOGI 3.アルティカセブン 4.ライブハウス代々木Barbara
             </p>
-            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/251897/widget" width="245"></iframe>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/256767/widget" width="245"></iframe>
+          </div>
+          <div class="column is-3 has-padding-30">
+            <p class="title is-5 is-spaced">
+              【ライブハウス救済プロジェクト】SAVE THE S.U.B TOKYO
+            </p>
+            <p class="subtitle is-6">
+              東京都中野坂上にあるライブハウスS.U.B TOKYOです。これまで様々なアイドル様アーティスト様にご利用頂いております。現在S.U.B TOKYOはコロナウイルス感染拡大防止のための政府からの自粛要請を受けまして、営業を自粛している状態です。
+            </p>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/260211/widget" width="245"></iframe>
+          </div>
+          <div class="column is-3 has-padding-30">
+            <p class="title is-5 is-spaced">
+              西荻窪w.jaz コロナを乗り越える！支援プロジェクト
+            </p>
+            <p class="subtitle is-6">
+              西荻窪w.jaz 通称だぶじゃず。コロナウィルスが猛威を振るう中、3月末から営業自粛しています。先が見えないこの状況ですが、必ず再起したいという思いで、大変心苦しいですが支援をお願いした次第です。
+            </p>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/258861/widget" width="245"></iframe>
+          </div>
+          <div class="column is-3 has-padding-30">
+            <p class="title is-5 is-spaced">
+              大久保bumpcity -バンプシティ- コロナに負けない！応援プロジェクト！
+            </p>
+            <p class="subtitle is-6">
+              大久保にあるライブバー、Bumpcity。バンプの名称でおなじみのあのバンプシティを救済するための応援プロジェクトです。13年目を迎えたバンプの火をまだまだ消したくない！皆さんの応援をお願いします。
+            </p>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/252906/widget" width="245"></iframe>
+          </div>
+          <div class="column is-3 has-padding-30">
+            <p class="title is-5 is-spaced">
+              Live Bar BACK BEATを、コロナ被害から助けよう！
+            </p>
+            <p class="subtitle is-6">
+              新型コロナウィルスの感染防止のため、営業自粛してます。その間にも、家賃やレンタルリース代等、いろいろお金はかかります。今後の営業存続のためにも、ご協力をよろしくお願い申し上げます。
+            </p>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/261373/widget" width="245"></iframe>
+          </div>
+          <div class="column is-3 has-padding-30">
+            <p class="title is-5 is-spaced">
+              LivePerformanceBar un.10応援プロジェクト
+            </p>
+            <p class="subtitle is-6">
+              コロナウィルスの影響により、今週から営業を自粛いたします四ツ谷荒木町に店を構えるLivePerformanceBar un.10の応援事業です。変わらず音楽を自由に楽しめる場所を失くさないために、どうぞよろしくお願いいたします。
+            </p>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/259532/widget" width="245"></iframe>
+          </div>
+          <div class="column is-3 has-padding-30">
+            <p class="title is-5 is-spaced">
+              三軒茶屋グレープフルーツムーン　緊急支援クラウドファンディング
+            </p>
+            <p class="subtitle is-6">
+              20年続いているGrapeFruitMoonの灯を消さない為に。これからも変らず皆様と笑顔溢れる空間を一緒に過ごせるように。今回のクラウドファンディングを立ち上げました。
+            </p>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/105794/widget" width="245"></iframe>
+          </div>
+          <div class="column is-3 has-padding-30">
+            <p class="title is-5 is-spaced">
+              ”STAY STRONG” 【新宿 DREAM STORE】支援プロジェクト
+            </p>
+            <p class="subtitle is-6">
+              私たちが大好きな【新宿 DREAM STORE】は新型コロナウィルス感染拡大防止のため営業することが出来ず、危機的な経営状況となっています。多くのライブハウスが店を潰さないように配信ライブなどの新しい体制を模索していますが、少しの期間、皆様の支援を必要としています。
+            </p>
+            <iframe frameborder="0" height="365" scrolling="no" src="https://camp-fire.jp/projects/261846/widget" width="245"></iframe>
           </div>
         </div>
       </div>
@@ -63,10 +140,13 @@
 
 <style>
 .hero.is-success {
-  background-color: #1d9c00;
+  background-color: #0a0a0a;
   color: #fff;
 }
 .body-title {
   margin-bottom: 5rem;
+}
+.has-padding-30 {
+  padding: 30px !important;
 }
 </style>
