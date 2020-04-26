261682603cdf9926daa7e279da50203905a2d365
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-04-26 19:45:45 +0900

    Extract statement section as component

--- pages/index.vue
+++ pages/index.vue
@@ -15,23 +15,7 @@
       </div>
     </section>
 
-    <section id="statement" class="section has-background-light">
-      <div class="container is-small has-text-centered">
-        <div class="columns">
-          <div class="column is-half is-offset-one-quarter">
-            <p>
-              現在コロナウイルスの影響で営業することができず、苦しんでいるライブハウスがたくさんあります。中にはすでに店を閉めざるを得ない選択をしたところもあります。<br>
-              <br>
-              このサイトはコロナウイルスで苦しんでいるライブハウスを支援するためのまとめサイトです。短期間でこんなに様々なプロジェクトが立ち上がっているのは本当に素晴らしいことだと思います。<br>
-              <br>
-              私自身がそうであったように、音楽などの文化は苦しい時にこそ必要とされるものだと思います。大切な音楽の灯を消さないために、少しでも支援していただけたら嬉しいです。<br>
-              <br>
-              誰かにとっての大切な場所がこれからもずっと存続できますように。
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Statement />
 
     <section id="support-sites" class="section has-background-white">
       <div class="container is-small has-text-centered">
@@ -119,11 +103,13 @@
 </template>
 
 <script>
+import Statement from '@/components/Statement.vue'
 import ProjectCard from '@/components/ProjectCard.vue'
 import projects from '@/assets/data/project.json'
 
 export default {
   components: {
+    Statement,
     ProjectCard
   },
   data () {
