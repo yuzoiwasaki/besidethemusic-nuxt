019554ead3be3b732789a39972b41f30d58c29a3
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-04-27 00:30:36 +0900

    Extract crowdfunding section as component

---
+++ components/Crowdfunding.vue
@@ -0,0 +1,41 @@
+<template>
+  <section id="crowdfunding" class="section has-background-white">
+    <div class="container is-small has-text-centered">
+      <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+          <h4 class="title is-spaced body-title">
+            現在クラウドファンディング実施中の東京のライブハウス一覧
+          </h4>
+        </div>
+      </div>
+      <div class="columns is-multiline">
+        <ProjectCard
+          v-for="project in projects"
+          :title="project.title"
+          :caption="project.caption"
+          :project-id="project.id"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ProjectCard from '@/components/ProjectCard.vue'
+import projects from '@/assets/data/project.json'
+
+export default {
+  name: 'Crowdfunding',
+  components: {
+    ProjectCard
+  },
+  data () {
+    return {
+      projects: projects
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,7 @@
     <Hero />
     <Statement />
     <SupportSite />
-
-    <section id="crowdfunding" class="section has-background-white">
-      <div class="container is-small has-text-centered">
-        <div class="columns">
-          <div class="column is-half is-offset-one-quarter">
-            <h4 class="title is-spaced body-title">
-              現在クラウドファンディング実施中の東京のライブハウス一覧
-            </h4>
-          </div>
-        </div>
-        <div class="columns is-multiline">
-          <ProjectCard
-            v-for="project in projects"
-            :title="project.title"
-            :caption="project.caption"
-            :project-id="project.id"
-          />
-        </div>
-      </div>
-    </section>
+    <Crowdfunding />
   </div>
 </template>
 
@@ -30,20 +11,14 @@
 import Hero from '@/components/hero.vue'
 import Statement from '@/components/Statement.vue'
 import SupportSite from '@/components/SupportSite.vue'
-import ProjectCard from '@/components/ProjectCard.vue'
-import projects from '@/assets/data/project.json'
+import Crowdfunding from '@/components/Crowdfunding.vue'
 
 export default {
   components: {
     Hero,
     Statement,
     SupportSite,
-    ProjectCard
-  },
-  data () {
-    return {
-      projects: projects
-    }
+    Crowdfunding
   }
 }
 </script>
