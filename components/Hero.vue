221bf0d6eee74c91fa8114ffd5a8d54f951b8751
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-06-14 12:19:30 +0900

    Implement header

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,24 @@
 <template>
   <section id="hero" class="hero-container">
+    <nav class="navbar has-background-black">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            Beside the Music
+          </a>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item" href="https://news.livedoor.com/article/detail/18402362" target="_blank">
+              お知らせ
+            </a>
+            <a class="navbar-item" href="https://twitter.com/besidethemusic" target="_blank">
+              Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
     <div id="mainVisual-pc" class="is-hidden-mobile"></div>
     <div id="mainVisual-sp" class="is-hidden-tablet"><img src="/images/main2_sp.jpg" width="100%"></div>
   </section>
@@ -16,5 +35,13 @@
 }
 #mainVisual-sp{
   background-color:#000000;
+}
+
+.navbar a {
+  color: #fff;
+}
+
+.navbar a:hover, a:visited {
+  background-color: #000;
 }
 </style>
