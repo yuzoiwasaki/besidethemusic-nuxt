2f93087f63b458dfa624d00050b3755fca571bf0
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-06-14 12:53:11 +0900

    Adapt transparent background

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,25 +1,26 @@
 <template>
   <section id="hero" class="hero-container">
-    <nav class="navbar has-background-black">
-      <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/">
-            <h1>Beside the Music</h1>
-          </a>
-        </div>
-        <div class="navbar-menu">
-          <div class="navbar-end">
-            <a class="navbar-item" href="https://news.livedoor.com/article/detail/18402362" target="_blank">
-              お知らせ
-            </a>
-            <a class="navbar-item" href="https://twitter.com/besidethemusic" target="_blank">
-              Twitter
+    <div id="mainVisual-pc" class="is-hidden-mobile">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+              <h1>Beside the Music</h1>
             </a>
           </div>
+          <div class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item" href="https://news.livedoor.com/article/detail/18402362" target="_blank">
+                お知らせ
+              </a>
+              <a class="navbar-item" href="https://twitter.com/besidethemusic" target="_blank">
+                Twitter
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
-    <div id="mainVisual-pc" class="is-hidden-mobile"></div>
+      </nav>
+    </div>
     <div id="mainVisual-sp" class="is-hidden-tablet"><img src="/images/main2_sp.jpg" width="100%"></div>
   </section>
 </template>
@@ -35,6 +36,10 @@
 }
 #mainVisual-sp{
   background-color:#000000;
+}
+
+.navbar {
+  background-color: transparent;
 }
 
 .navbar a {
