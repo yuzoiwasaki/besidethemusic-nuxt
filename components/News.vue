3db47461bd1cf2570dbd308ef386ba07f82fd10c
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-06-28 00:11:18 +0900

    Refactor

--- components/News.vue
+++ components/News.vue
@@ -11,24 +11,34 @@
       <div class="columns">
         <div class="column is-half is-offset-one-quarter">
           <ul>
-            <li class="entry">
-              <a href="http://save-our-space.org/tokyogovernorelection2020/" target="_blank">2020/06/24：【東京都知事選候補者へ聞きたいこと】縮小営業を余儀なくされているライブハウス/クラブに関しての対応を聞きました。</a>
-            </li>
-            <li class="entry">
-              <a href="https://mainichi.jp/articles/20200619/k00/00m/040/336000c" target="_blank">2020/06/19：「とにかく長かった」　都内のライブハウス、休業要請全面解除で3カ月ぶり再開</a>
-            </li>
-            <li class="entry">
-              <a href="https://mainichi.jp/articles/20200616/ddl/k13/040/009000c" target="_blank">2020/06/16：新型コロナ　ライブハウス助成へ　杉並区、法人最大４０万円　／東京</a>
-            </li>
-            <li class="entry">
-              <a href="https://news.yahoo.co.jp/articles/bcea0536ec53f94ad4f9b27a6ed2510350be7fd9" target="_blank">2020/06/11：接待伴う店やライブハウス19日に可　東京アラート解除</a>
-            </li>
+            <NewsList
+              title="2020/06/24：【東京都知事選候補者へ聞きたいこと】縮小営業を余儀なくされているライブハウス/クラブに関しての対応を聞きました。"
+              url="http://save-our-space.org/tokyogovernorelection2020"
+            />
+            <NewsList
+              title="2020/06/19：「とにかく長かった」　都内のライブハウス、休業要請全面解除で3カ月ぶり再開"
+              url="https://mainichi.jp/articles/20200619/k00/00m/040/336000c"
+            />
+            <NewsList
+              title="2020/06/16：新型コロナ　ライブハウス助成へ　杉並区、法人最大４０万円　／東京"
+              url="https://mainichi.jp/articles/20200616/ddl/k13/040/009000c"
+            />
           </ul>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+import NewsList from '@/components/NewsList.vue'
+
+export default {
+  components: {
+    NewsList
+  }
+}
+</script>
 
 <style>
 .entry {
---
+++ components/NewsList.vue
@@ -0,0 +1,26 @@
+<template>
+  <li class="entry">
+    <a :href="url" target="_blank">{{ title }}</a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      requred: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.entry {
+  margin: 30px 0;
+}
+</style>
